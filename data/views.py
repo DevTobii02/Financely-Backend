139b751c845800c21aba1aec01a5be91d4acd4ef
Adebayo Abdullahi <tobiithedev@gmail.com>
2026-03-31 15:22:06 +0100

    views for data app

--- data/views.py
+++ data/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import permissions, serializers, viewsets
 
-# Create your views here.
+from .models import DataImport
+
+
+class DataImportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DataImport
+        fields = "__all__"
+
+
+class DataImportViewSet(viewsets.ModelViewSet):
+    serializer_class = DataImportSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return DataImport.objects.filter(user=self.request.user)
