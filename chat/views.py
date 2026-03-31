519188a1da21afadc67c1cb63bc580395cf2c703
Adebayo Abdullahi <tobiithedev@gmail.com>
2026-03-31 15:19:42 +0100

    views for chat app

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import permissions, serializers, viewsets
 
-# Create your views here.
+from .models import ChatMessage
+
+
+class ChatMessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ChatMessage
+        fields = "__all__"
+
+
+class ChatMessageViewSet(viewsets.ModelViewSet):
+    serializer_class = ChatMessageSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return ChatMessage.objects.filter(user=self.request.user)
