6dc3659db51e0873c53cbbdf6de59ef7ee62765d
Adebayo Abdullahi <tobiithedev@gmail.com>
2026-03-31 15:16:36 +0100

    views for budgets app

--- budgets/views.py
+++ budgets/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import permissions, serializers, viewsets
 
-# Create your views here.
+from .models import Budget
+
+
+class BudgetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Budget
+        fields = "__all__"
+
+
+class BudgetViewSet(viewsets.ModelViewSet):
+    serializer_class = BudgetSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Budget.objects.filter(user=self.request.user)
