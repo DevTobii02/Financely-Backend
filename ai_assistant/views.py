5123d1577be5f5b6605733ca68c6a97ec2bb2df7
Adebayo Abdullahi <tobiithedev@gmail.com>
2026-03-31 14:41:11 +0100

    views for ai_assistant app

--- ai_assistant/views.py
+++ ai_assistant/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import permissions, serializers, viewsets
 
-# Create your views here.
+from .models import Recommendation
+
+
+class RecommendationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Recommendation
+        fields = "__all__"
+
+
+class RecommendationViewSet(viewsets.ModelViewSet):
+    serializer_class = RecommendationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Recommendation.objects.filter(user=self.request.user)
