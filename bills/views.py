63478e3c70a6c0e1994fd4f2d06f0a73d84aeb9d
Adebayo Abdullahi <tobiithedev@gmail.com>
2026-03-31 15:15:33 +0100

    vies for bills app

--- bills/views.py
+++ bills/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import permissions, serializers, viewsets
 
-# Create your views here.
+from .models import Bill, BillPayment
+
+
+class BillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bill
+        fields = "__all__"
+
+
+class BillPaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BillPayment
+        fields = "__all__"
+
+
+class BillViewSet(viewsets.ModelViewSet):
+    serializer_class = BillSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Bill.objects.filter(user=self.request.user)
+
+
+class BillPaymentViewSet(viewsets.ModelViewSet):
+    serializer_class = BillPaymentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return BillPayment.objects.filter(user=self.request.user)
