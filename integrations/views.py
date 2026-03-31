ea5fefc05fad1731f979e45c6076018949816d2c
Adebayo Abdullahi <tobiithedev@gmail.com>
2026-03-31 15:31:01 +0100

    for integrating previous banking systems

--- integrations/views.py
+++ integrations/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import permissions, serializers, viewsets
 
-# Create your views here.
+from .models import Integration
+
+
+class IntegrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Integration
+        fields = "__all__"
+
+
+class IntegrationViewSet(viewsets.ModelViewSet):
+    serializer_class = IntegrationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Integration.objects.filter(user=self.request.user)
