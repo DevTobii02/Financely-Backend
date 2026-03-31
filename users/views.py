b07a366d1498a0bb6d78a18975a39805e3f39799
Adebayo Abdullahi <tobiithedev@gmail.com>
2026-03-31 15:42:41 +0100

    views users app

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import permissions, serializers, viewsets
 
-# Create your views here.
+from .models import CustomUser
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = "__all__"
+
+
+class CustomUserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+    permission_classes = [permissions.IsAuthenticated]
