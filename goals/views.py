02b8d3626be6b17105b4b5e36ec151381e766748
Adebayo Abdullahi <tobiithedev@gmail.com>
2026-03-31 15:28:48 +0100

    app for user defined goals

--- goals/views.py
+++ goals/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import permissions, serializers, viewsets
 
-# Create your views here.
+from .models import Goal
+
+
+class GoalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Goal
+        fields = "__all__"
+
+
+class GoalViewSet(viewsets.ModelViewSet):
+    serializer_class = GoalSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Goal.objects.filter(user=self.request.user)
