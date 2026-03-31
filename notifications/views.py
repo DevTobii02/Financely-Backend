a03146defc0a4d56bc19da5fbecefacf11905c8a
Adebayo Abdullahi <tobiithedev@gmail.com>
2026-03-31 15:35:47 +0100

    notification views for notification app

--- notifications/views.py
+++ notifications/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import permissions, serializers, viewsets
 
-# Create your views here.
+from .models import Notification
+
+
+class NotificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Notification
+        fields = "__all__"
+
+
+class NotificationViewSet(viewsets.ModelViewSet):
+    serializer_class = NotificationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Notification.objects.filter(user=self.request.user)
