1b07cb47197e5d0f5ff7eb0dfc5c75b6bc285b5d
Adebayo Abdullahi <tobiithedev@gmail.com>
2026-03-31 15:33:24 +0100

    for machine learning models

--- ml_engine/views.py
+++ ml_engine/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import permissions, serializers, viewsets
 
-# Create your views here.
+from .models import MLModelLog
+
+
+class MLModelLogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MLModelLog
+        fields = "__all__"
+
+
+class MLModelLogViewSet(viewsets.ModelViewSet):
+    serializer_class = MLModelLogSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return MLModelLog.objects.filter(user=self.request.user)
