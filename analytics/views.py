7b7d6a272dfb6135a52a72126961758223e61e67
Adebayo Abdullahi <tobiithedev@gmail.com>
2026-03-31 15:12:48 +0100

    views for analytics app

--- analytics/views.py
+++ analytics/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import permissions, serializers, viewsets
 
-# Create your views here.
+from .models import SpendingSummary
+
+
+class SpendingSummarySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SpendingSummary
+        fields = "__all__"
+
+
+class SpendingSummaryViewSet(viewsets.ModelViewSet):
+    serializer_class = SpendingSummarySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return SpendingSummary.objects.filter(user=self.request.user)
